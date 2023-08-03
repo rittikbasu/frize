1d5ad35d0b48b9fc49b16004ac5e7183152d97a7
rittikbasu <irittik@gmail.com>
2023-08-03 23:14:14 +0530

    feat: rm search box + change styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,24 +31,17 @@ function ModeToggle() {
       onClick={toggleMode}
     >
       <BsMoon className="h-[1.3rem] w-[1.3rem] fill-zinc-500 transition dark:hidden md:hover:fill-lime-400" />
-      <BsSun className="hidden h-6 w-6 fill-lime-400 transition dark:block md:hover:fill-lime-600" />
+      <BsSun className="hidden h-6 w-6 fill-gray-400 transition dark:block md:hover:fill-orange-400" />
     </button>
   );
 }
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 right-0 px-4 py-2 backdrop-blur flex items-center justify-between m-2 max-w-5xl xl:mx-auto">
-      <div className="flex items-center flex-grow">
-        <div className="text-2xl text-green-400 dark:text-lime-400 tracking-wider">
+    <nav className="fixed top-0 left-0 right-0 -backdrop-hue-rotate-90 backdrop-blur-sm z-50 px-4 py-2 flex items-center justify-between max-w-5xl xl:mx-auto">
+      <div className="flex items-center m-2 flex-grow">
+        <div className="text-2xl text-red-400 dark:text-lime-400 font-mono tracking-wider">
           frize
-        </div>
-        <div className="bg-gray-100 dark:bg-zinc-700 rounded-2xl py-1 px-4 mx-4 lg:mx-32 flex-grow">
-          <input
-            type="text"
-            placeholder="Search"
-            className="bg-transparent focus:outline-none"
-          />
         </div>
       </div>
       <div className="flex items-center space-x-2">
