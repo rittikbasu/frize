250d3da4def5fcc62d3b99239cdccff7ec08e108
rittikbasu <irittik@gmail.com>
2023-08-12 10:16:58 +0530

    feat: format legend titles

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -2,24 +2,52 @@ import { Card, Title, LineChart } from "@tremor/react";
 import { timeFormatter } from "@/utils/timeFormatter";
 
 export default function Chart(chartData) {
-  const keys = Object.keys(chartData.chartData[0]).filter(
-    (key) => key !== "date" && key !== "categories"
+  // change the names of keys in the object chartData to uppercase
+  const data = chartData.chartData.map((item) => {
+    const newItem = {};
+    Object.keys(item).forEach((key) => {
+      let newKey;
+      if (key === "nonwork_categories") {
+        newKey = "Non-work hours";
+      } else {
+        // key to title case
+        newKey = key
+          .split("_")
+          .map((word) => word[0].toUpperCase() + word.slice(1))
+          .join(" ");
+      }
+      newItem[newKey] = item[key];
+    });
+    return newItem;
+  });
+
+  console.log(data);
+
+  const keys = Object.keys(data[0]).filter(
+    (key) =>
+      key !== "Date" &&
+      key !== "Categories" &&
+      key !== "Work Categories" &&
+      key !== "Day"
   );
+
   return (
     <Card className="">
       <Title>Productivity Graph</Title>
       <LineChart
         className="mt-6"
-        data={chartData.chartData}
-        index="date"
+        data={data}
+        index="Date"
         categories={keys}
-        colors={["lime", "gray", "orange", "pink", "violet"]}
+        colors={["lime", "gray", "orange", "violet"]}
         valueFormatter={timeFormatter}
         yAxisWidth={26}
         // showLegend={false}
         curveType="monotone"
         // showYAxis={false}
+        // showXAxis={false}
         // showTooltip={false}
+        // showGridLines={false}
       />
     </Card>
   );
