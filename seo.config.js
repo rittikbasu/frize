cb01a179622e4e5f589e7743fba46d869ba30470
rittikbasu <irittik@gmail.com>
2023-08-20 04:35:21 +0530

    feat: add next-seo

---
+++ seo.config.js
@@ -0,0 +1,39 @@
+export default {
+  defaultTitle: 'frize | Productivity Dashboard',
+  description:
+    "An interactive dashboard that transforms your Rize data into beautiful charts and graphs.",
+//   openGraph: {
+//     title: 'frize',
+//     description:
+//       "An interactive dashboard that transforms your Rize data into beautiful charts and graphs.",
+//     images: [
+//       {
+//         url: '',
+//         width: 1200,
+//         height: 600,
+//         alt: '',
+//       },
+//     ],
+//     type: 'website',
+//     locale: 'en_US',
+//     url: 'https://frize.rittik.io',
+//     site_name: 'frize',
+//   },
+//   twitter: {
+//     cardType: 'summary_large_image',
+//   },
+  additionalMetaTags: [
+    {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
+    },
+    {
+        name: 'apple-mobile-web-app-capable',
+        content: 'yes',
+    },
+    {
+        name: 'apple-mobile-web-app-status-bar-style',
+        content: 'black',
+    },
+],
+}
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,15 +2,13 @@ import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
+import { DefaultSeo } from "next-seo";
+import SEO from "../../seo.config";
 
 export default function App({ Component, pageProps }) {
   return (
     <main>
-      <div className="fixed inset-0 flex justify-center sm:px-8">
-        <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="w-full bg-white dark:bg-black" />
-        </div>
-      </div>
+      <DefaultSeo {...SEO} />
       <div className="relative selection:bg-lime-500 selection:text-white dark:selection:bg-indigo-800">
         <Navbar />
         <main>
