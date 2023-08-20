b808fee832dbb258252cb0166f782b969583126c
rittikbasu <irittik@gmail.com>
2023-08-20 06:05:52 +0530

    feat: add og image

--- seo.config.js
+++ seo.config.js
@@ -2,26 +2,26 @@ export default {
   defaultTitle: 'frize | Productivity Dashboard',
   description:
     "An interactive dashboard that transforms your Rize data into beautiful charts and graphs.",
-//   openGraph: {
-//     title: 'frize',
-//     description:
-//       "An interactive dashboard that transforms your Rize data into beautiful charts and graphs.",
-//     images: [
-//       {
-//         url: '',
-//         width: 1200,
-//         height: 600,
-//         alt: '',
-//       },
-//     ],
-//     type: 'website',
-//     locale: 'en_US',
-//     url: 'https://frize.rittik.io',
-//     site_name: 'frize',
-//   },
-//   twitter: {
-//     cardType: 'summary_large_image',
-//   },
+  openGraph: {
+    title: 'frize',
+    description:
+      "An interactive dashboard that transforms your Rize data into beautiful charts and graphs.",
+    images: [
+      {
+        url: 'https://ik.imagekit.io/zwcfsadeijm/frizeOG_aV7PhUD_0.JPG?updatedAt=1692491298986',
+        width: 1200,
+        height: 600,
+        alt: 'frize | Productivity Dashboard',
+      },
+    ],
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://frize.rittik.io',
+    site_name: 'frize',
+  },
+  twitter: {
+    cardType: 'summary_large_image',
+  },
   additionalMetaTags: [
     {
         name: 'viewport',
